e4ca9698a740311e2dde1a343478421960ba8319
erind <erindmehmeti@gmail.com>
2023-08-10 18:43:16 +0200

    added tabs

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,19 +1,46 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
-export class ModalService {
+export class  ModalService {
+  private modals: IModal[] = []
 
-  private visible: Boolean = false
+  // private visible: Boolean = false
 
   constructor() { }
 
-  isModalOpen() {
-    return this.visible;
+  isModalOpen(id: string): boolean {
+    return Boolean(this.modals.find(element => element.id === id)?.visible);
+    // return !!this.modals.find(element => element.id === id)?.visible;
   }
 
-  toggleModal() {
-    this.visible = !this.visible;
+  toggleModal(id: string) {
+    const modal = this.modals.find(element => element.id === id)
+    // this.visible = !this.visible;
+    if (modal) {
+      modal.visible = !modal.visible;
+    }
+  }
+
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false
+    })
+
+    console.log(this.modals);
+  }
+
+  unregister(id: string){
+    this.modals = this.modals.filter(
+      element => element.id !== id
+    )
   }
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,13 +6,20 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent {
-closeModal() {
-  this.modal.toggleModal();
-}
 
-  constructor(public modal: ModalService) {
+export class ModalComponent implements OnInit {
+  @Input() modalID = ''
 
+  constructor(public modal: ModalService, public el: ElementRef) {
+     console.log(el); 
+  }
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID);
   }
 
 }
